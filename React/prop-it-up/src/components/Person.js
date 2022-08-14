5cae1750ab88b968b74f4e1902f775be82a3bed2
Cory Duffy <duffcory@gmail.com>
2022-08-14 11:34:21 -0700

    added state to form

--- React/prop-it-up/src/components/Person.js
+++ React/prop-it-up/src/components/Person.js
@@ -1,11 +1,13 @@
-import React from 'react';
+import React, {useState} from 'react';
 
 const Person = (props) => {
+    const [newBirthday, setNewBirthday] = useState(props.age);
     return (
         <div>
             <h2>{props.lastname}, {props.firstname}</h2>
-            <p>Age:{props.age}</p>
+            <p>Age:{newBirthday}</p>
             <p>Hair Color:{props.haircolor}</p> 
+            <button onClick={ (event) => setNewBirthday(newBirthday + 1)}>Birthday Button for {props.firstname}{props.lastname}</button>
         </div> 
         )
 }
