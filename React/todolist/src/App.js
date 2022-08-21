9bc26fa5ba347485dc80919ed8093ba82a0cad58
Cory Duffy <duffcory@gmail.com>
2022-08-21 10:00:43 -0700

    start of todo

---
+++ React/todolist/src/App.js
@@ -0,0 +1,18 @@
+import logo from './logo.svg';
+import './App.css';
+import Form from './components/Form';
+import List from './components/List';
+import Todo from './components/Todo';
+
+
+function App() {
+  return (
+    <div className="App">
+      <Form/>>
+      <List/>>
+      
+    </div>
+  );
+}
+
+export default App;
---
+++ React/todolist/src/components/Form.js
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const Form = () => {
+  return (
+    <div>
+        <form>
+            <div>
+                <label htmlFor="">Todo:</label>
+                <input type="text" />
+            </div>
+            <div><button>Submit</button></div>    
+        </form>
+    </div>
+  )
+}
+
+export default Form
