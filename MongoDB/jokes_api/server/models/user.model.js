87353616db78da904f3b8bc65b88bb0e4665ec12
Cory Duffy <duffcory@gmail.com>
2022-09-10 14:48:54 -0700

    added timestamps

--- MongoDB/jokes_api/server/models/user.model.js
+++ MongoDB/jokes_api/server/models/user.model.js
@@ -2,15 +2,11 @@ const mongoose = require('mongoose');
 
 //creates blueprint for db object
 const UserSchema = new mongoose.Schema({
-    name: {
-        type: String
-    },
-    age: {
-        type: Number
-    }
-});
+    name: {type: String},
+    age: {type: Number}
+}, {timestamps: true});
 
 //creates nexessary db
 const User = mongoose.model('User', UserSchema);
- 
+
 module.exports = User;
