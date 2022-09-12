11a8ad941c9a4dcdedcd0b06320d7959b62094d8
Cory Duffy <duffcory@gmail.com>
2022-09-12 12:26:03 -0700

    basic front end

--- FullStack/myNewProject/server/server.js
+++ FullStack/myNewProject/server/server.js
@@ -1,5 +1,7 @@
 const express = require('express');
+const cors = require('cors')
 const app = express();
+app.use(cors())
 require('./routes/person.routes')(app); //We are importing the routes export
 const port = 8000;
     
